7346b14fad9fe32bfc5ba85308beae59652bd3ba
Oscar Wales <oscarjwales@gmail.com>
2020-08-04 16:27:52 +0100

    Classes challenge 5 completed

---
+++ day_2/classes/05_string.php
@@ -0,0 +1,51 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+class Stringy
+{
+    private $string;
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    public function lower()
+    {
+        return strtolower($this->string);
+    }
+
+    public function upper()
+    {
+        return strtoupper($this->string);
+    }
+
+    public function append($str)
+    {
+        return "{$this->string}{$str}";
+    }
+
+    public function repeat($num)
+    {
+        $rep = "";
+        for ($i = 0; $i < $num; $i += 1){
+            $rep .= $this->string;
+        }
+        return $rep;
+    }
+}
+
+$string = new Stringy("Na");
+
+// it can lowercase a string
+dump($string->lower()); // "na"
+
+// it can uppercase a string
+dump($string->upper()); // "NA"
+
+// it can concatenate
+dump($string->append("blah")); // "Nablah"
+
+// it can repeat a string
+dump($string->repeat(5)); // "NaNaNaNaNa"
